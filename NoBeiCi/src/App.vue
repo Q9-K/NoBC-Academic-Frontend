cad4d2160576f8fe9e34024462163b9af1adcf5a
Q9-K <20374319@buaa.edu.cn>
2023-09-14 00:25:38 +0800

    ✨ feat:test for ci

--- NoBeiCi/src/App.vue
+++ NoBeiCi/src/App.vue
@@ -1,3 +1,8 @@
+<!--
+ * @Date: 2023-09-14 00:16:18
+ * @Author: Q9K
+ * @Description: 
+-->
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
--- NoBeiCi/src/main.js
+++ NoBeiCi/src/main.js
@@ -1,10 +1,15 @@
+/*
+ * @Date: 2023-09-14 00:15:07
+ * @Author: Q9K
+ * @Description: 
+ */
 import { createApp } from 'vue'
-// import './style.css'
+import './style.css'
 import App from './App.vue'
 import axios from 'axios'
-import router from 'vue-router'
+import router from './router'
 
 
 const app = createApp(App)
-
+app.use(router)
 app.mount('#app')
