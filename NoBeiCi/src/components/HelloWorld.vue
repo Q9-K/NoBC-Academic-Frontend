35a3b388acdceda223f22df01fa16b1e9b2ce1b7
KeineWieDu <578586993@qq.com>
2023-09-25 11:23:01 +0800

    feat: 添加tailwind css依赖

--- NoBeiCi/src/components/HelloWorld.vue
+++ NoBeiCi/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import '../style.css'
 
 defineProps({
   msg: String,
@@ -13,7 +14,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <p class="text-3xl font-bold underline">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
--- NoBeiCi/src/main.js
+++ NoBeiCi/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import axios from 'axios'
-import router from 'vue-router'
+// import router from 'vue-router'
 
 
 const app = createApp(App)
