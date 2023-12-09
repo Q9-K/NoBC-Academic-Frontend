2c8de65b6d5acf7dc7b688aae7fd9901ea8b2296
KeineWieDu <578586993@qq.com>
2023-12-09 23:28:28 +0800

    feat: 一个远程搜索领域的栗子

--- NoBeiCi/src/App.vue
+++ NoBeiCi/src/App.vue
@@ -1,18 +1,41 @@
 <!--
  * @Date: 2023-09-14 00:16:18
  * @Author: Q9K
- * @Description: 
+ * @Description:
 -->
 <script setup>
 import Beian from './components/Beian.vue'
+import SearchBar from "./components/SearchBar.vue";
+import axios from "axios";
+import {useUpperSearchBarStore} from "./stores/upperSearchBar.js";
+
+const search = (value) => {
+  return axios.get('http://100.92.185.118:8000' + '/concept/search_concept/', {
+    params: {
+      keyword: value,
+      language: 1
+    }
+  }).then((response) => {
+    if (response.status === 200) {
+      const data = response.data.data
+      console.log(data)
+      const upperSearchBar = useUpperSearchBarStore();
+      for (let {id, chinese_display_name} of data) {
+        upperSearchBar.addIntoOptions(id, chinese_display_name)
+        console.log({id, chinese_display_name})
+      }
+    }
+  })
+}
 </script>
 
 <template>
-  
+
   <div >
-    
-    <router-view></router-view>
+
+<!--    <router-view></router-view>-->
     <!-- <Beian></Beian> -->
+    <SearchBar :info="'can u see me'" :search-function="search"/>
   </div>
 </template>
 
