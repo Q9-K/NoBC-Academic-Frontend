fcec9415327edae6e0057776939d98352ece9612
KeineWieDu <578586993@qq.com>
2023-12-24 10:44:28 +0800

    feat: 完善新用户选择关注领域

--- NoBeiCi/src/views/home/SelectField.vue
+++ NoBeiCi/src/views/home/SelectField.vue
@@ -6,6 +6,14 @@ import i18n from "../../locales/index.js";
 import {handleResponse} from "../../functions/handleResponse.js";
 import * as echarts from 'echarts'
 import {FINISH, useStateOfPriorDialog} from "../../stores/stateOfPriorDialog.js";
+import {debounce} from "vue-debounce";
+import request from '../../functions/Request.js'
+import {ElMessage} from "element-plus";
+
+const currentSelectedFieldId = ref('')
+const currentSelectedFieldName = ref('')
+let currentSelectFieldEnName = ''
+let currentSelectFiledCnName = ''
 
 const stateOfPriorDialog = useStateOfPriorDialog()
 
@@ -20,23 +28,25 @@ onMounted(() => {
     handleResponse(response, false, (data) => {
 
       for (let {display_name, chinese_display_name, id} of data) {
-        cnFieldsData.push({
+        const cnField = {
           name: chinese_display_name,
           value: 1,
           children: [],
           id: id
-        })
+        }
+        cnFieldsData.push(cnField)
 
-        enFieldsData.push({
+        const enField = {
           name: display_name,
           value: 1,
           children: [],
           id: id
-        })
+        }
+        enFieldsData.push(enField)
 
         idFieldMap.set(id, {
-          cnFields: cnFieldsData,
-          enFields: enFieldsData
+          cnField: cnField,
+          enField: enField
         })
       }
     })
@@ -45,35 +55,41 @@ onMounted(() => {
   let getLevel1
   Promise.all([getLevel0])
     .then(() => {
-      for (let {parentId} of cnFieldsData) {
+      for (let {id} of cnFieldsData) {
+        let parentId = id
         getLevel1 = axios.get('http://100.92.185.118:8000' + '/concept/get_subdomains/', {
           params: {
-            id: parentId
+            id: id
           }
-        }).then((response) => {
+        })
+          .then((response) => {
 
           console.log("1")
 
           handleResponse(response, false, (data) => {
 
             for (let {display_name, chinese_display_name, id} of data) {
-              console.log(cnFieldsData)
-              console.log(idFieldMap)
-              console.log(id)
-              const {cnParentField, enParentField} = idFieldMap.get(parentId)
+              const {cnField, enField} = idFieldMap.get(parentId)
 
-              cnParentField.children.push({
+              const cnChildField = {
                 name: chinese_display_name,
                 value: 1,
                 children: [],
                 id: id
-              })
+              }
+              cnField.children.push(cnChildField)
 
-              enParentField.children.push({
+              const enChildField = {
                 name: display_name,
                 value: 1,
                 children: [],
                 id: id
+              }
+              enField.children.push(enChildField)
+
+              idFieldMap.set(id, {
+                cnField: cnChildField,
+                enField: enChildField
               })
             }
           })
@@ -137,6 +153,11 @@ onMounted(() => {
 
             rectangleTree.hideLoading()
             rectangleTree.setOption(options)
+
+            rectangleTree.on('click', debounce((params) => {
+              console.log(params)
+              currentSelectedFieldId.value = params.data.id
+            }, "100ms"))
           })
       }
   })
@@ -147,9 +168,11 @@ watch(
   (newValue) => {
     if (newValue === 'en') {
       fields = enFieldsData
+      currentSelectedFieldName.value = currentSelectFieldEnName
     }
     else {
       fields = cnFieldsData
+      currentSelectedFieldName.value = currentSelectFiledCnName
     }
     rectangleTree.setOption({
       series: {
@@ -167,19 +190,95 @@ const handleFinishSelect = () => {
   stateOfPriorDialog.setStep(2)
   stateOfPriorDialog.setView(FINISH)
 }
+
+watch(
+  () => currentSelectedFieldId.value,
+  (newValue) => {
+    const {cnField, enField} = idFieldMap.get(newValue)
+    currentSelectFiledCnName = cnField.name
+    currentSelectFieldEnName = enField.name
+    if (i18n.getLocale() === 'en') {
+      currentSelectedFieldName.value = currentSelectFieldEnName
+    }
+    else {
+      currentSelectedFieldName.value = currentSelectFiledCnName
+    }
+  }
+)
+
+const handleSelectField = () => {
+
+  const selectField = (fieldId) => {
+    try {
+      const apiUrl = 'http://100.92.185.118:8000' + '/user/add_focus_concept/'
+      const params = {
+        concept_id: fieldId
+      }
+
+      const response = request({
+        url: apiUrl,
+        params: params,
+        showLoading: true,
+        addToken: true,
+        errorCallback: null,
+        showError: true
+      })
+
+      if (response) {
+        return response
+      }
+      else {
+        console.log("空")
+      }
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  selectField(currentSelectedFieldId.value)
+    .then((response) => {
+      if (response.code !== 200) {
+        ElMessage({
+          type: "error",
+          message: "Oh No!"
+        })
+      }
+      else {
+        ElMessage({
+          type: "success",
+          message: "OK!"
+        })
+      }
+    })
+}
+
 </script>
 
 <template>
   <div class="select-field-outer">
     <div class="select-field">
       <div class="select-field-title-outer">
-        <el-select/>
+        <el-input
+          class="select-field-title"
+          v-model="currentSelectedFieldName"
+          disabled="disabled"
+        >
+          <template #append>
+            <el-button type="primary" @click="handleSelectField">
+              {{ i18n.t('priorDialog.selectFieldMode.selectButton') }}
+            </el-button>
+          </template>
+        </el-input>
       </div>
       <div class="select-field-rectangle-tree-outer">
         <div id="containerOfRectangleTree" class="select-field-rectangle-tree"></div>
       </div>
     </div>
     <div class="finish-select-outer">
+      <el-button class="skip-select" @click="handleFinishSelect">
+        {{ i18n.t('priorDialog.selectFieldMode.skipSelect') }}
+      </el-button>
       <el-button type="primary" class="finish-select" @click="handleFinishSelect">
         {{ i18n.t('priorDialog.selectFieldMode.finishSelect') }}
       </el-button>
@@ -201,6 +300,15 @@ const handleFinishSelect = () => {
     .select-field-title-outer {
       width: 100%;
       height: 15%;
+      display: flex;
+      justify-content: center;
+      align-items: start;
+      .select-field-title {
+        width: 50%;
+        height: 80%;
+        position: relative;
+        bottom: 14px;
+      }
     }
     .select-field-rectangle-tree-outer {
       width: 100%;
@@ -220,6 +328,11 @@ const handleFinishSelect = () => {
     display: flex;
     justify-content: end;
     align-items: center;
+    .skip-select {
+      height: 100%;
+      position: relative;
+      right: 50px;
+    }
     .finish-select {
       height: 100%;
       position: relative;
