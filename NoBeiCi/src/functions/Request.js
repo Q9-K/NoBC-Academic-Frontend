afb2884261ed223b1b13b3d10ea26aa49e629396
KeineWieDu <578586993@qq.com>
2023-12-25 21:26:44 +0800

    feat: 使用域名

--- NoBeiCi/src/functions/Request.js
+++ NoBeiCi/src/functions/Request.js
@@ -8,7 +8,7 @@ const contentTypeJson = "application/json"
 
 const instance = axios.create({
     timeout: 10 * 1000,
-    baseURL: "http://123.60.99.8:",
+    baseURL: "http://api.buaa-q9k.xyz",
 })
 //请求前过滤器
 let loading = null;
