374cc166121409bea0e89838e56c35d86cdb26d7
KeineWieDu <578586993@qq.com>
2023-12-26 14:22:57 +0800

    feat: 站内信完成

--- NoBeiCi/src/components/NavigateBar.vue
+++ NoBeiCi/src/components/NavigateBar.vue
@@ -182,13 +182,17 @@ const handleOpenMailDrawer = () => {
 }
 
 watch(
-  () => localStorage.getItem('userInformation'),
+  () => {
+    const stateOfPriorDialog = useStateOfPriorDialog()
+    return stateOfPriorDialog.getVisible
+  },
   (newValue) => {
-    if (newValue === null) {
+    if (newValue === true) {
       return
     }
     const email = JSON.parse(localStorage.getItem('userInformation')).email
     const idEmail = email.split('@')[0]
+    console.log(idEmail)
     const mailWebSocket = new WebSocket('ws://api.buaa-q9k.xyz/ws/' + idEmail + '/message')
     mailWebSocket.onmessage = (event) => {
       whetherShowDot.value = 1
