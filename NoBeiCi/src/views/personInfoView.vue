e5f9897557b65d0085d518e16d393e9a1bdb83e8
morty-idhyuajjcbdchjdehsr <326855092@qq.com>
2023-12-26 12:00:42 +0800

    修改用户头像bug
    1111

--- NoBeiCi/src/views/personInfoView.vue
+++ NoBeiCi/src/views/personInfoView.vue
@@ -225,7 +225,7 @@
 
                 <el-card shadow="hover" style="width: 100%;">
                 <div style="display: flex; align-items: center;margin-left: 40%;">
-                  <el-avatar :size="80" :round="true" :src="this.user_avatar" ></el-avatar>
+                  <el-avatar :size="80" :round="true" :src="user_avatar" ></el-avatar>
                   
                 </div>
                 </el-card>
