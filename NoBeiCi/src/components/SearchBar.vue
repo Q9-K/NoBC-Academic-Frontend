f1f55e71d49955ed0e65d78fc81457196dc7fb2b
KeineWieDu <578586993@qq.com>
2023-12-26 19:19:16 +0800

    feat: 确切写出论文总数

--- NoBeiCi/src/components/SearchBar.vue
+++ NoBeiCi/src/components/SearchBar.vue
@@ -35,7 +35,7 @@
         </div>
         <div class="data">
           <div class="num">
-            240,000,000
+            246,328,942
           </div>
           <div class="name">
             {{ i18n.t("scholar.thesis") }}
